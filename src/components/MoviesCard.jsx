61da9947014eeb63d866c1a1c1d488649a9dd93d
Claudio D. Morales <mclaudiod@gmail.com>
2024-01-12 18:18:46 -0300

    some more work on the cards with tailwind

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -4,10 +4,10 @@ export const MoviesCard = ({ movie }) => {
   const imgURL = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
 
   return (
-    <li className="rounded border-2 border-black">
-      <Link className="flex flex-col justify-center" to={`/movie/${movie.id}`}>
-        <img className="" src={imgURL} alt={movie.title} />
-        <div className="text-center">{movie.title}</div>
+    <li className="flex flex-col rounded border-4 border-black overflow-hidden shadow-lg shadow-black">
+      <Link className="grow flex flex-col" to={`/movie/${movie.id}`}>
+        <img className="border-b-4 border-black" src={imgURL} alt={movie.title} />
+        <div className="text-lg font-bold text-center p-6">{movie.title}</div>
       </Link>
     </li>
   );
